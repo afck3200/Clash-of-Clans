a91c20ceab5e38e771f654a0088808a304bb5daa
Vincent Yu <vinceyue667@gmail.com>
2015-06-30 14:05:51 +0800

    Clash-of-Clans

--- Assets/Scripts/Selector.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class Selector : MonoBehaviour {	//many game items need an index and a isSelected flag
-	
-	public bool isSelected = true;
-	public int index = -1;
-
-}
